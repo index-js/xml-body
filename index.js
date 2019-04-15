f26730a91361e37e645307951a07fd18b086e4ae
Yanglin <i@yangl.in>
2019-04-15 18:52:25 +0800

    Change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = options => {
   return async (ctx, next) => {
     let type = ctx.headers['content-type'] || ''
 
-    if (type.match(/\/xml\b/) && ctx.method !== 'GET') ctx.request.body = await parser(ctx.req)
+    if (type.match(/\/xml\b/) && ctx.method !== 'GET') ctx.request.body = await parser(ctx.req, options)
 
     return next()
   }
